23d60cd88c70fcc746c78db520ff4562bfd5f381
taha-tech-2003 <majorcop17@gmail.com>
2025-08-06 21:15:20 +0500

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en" className="bg-app" >
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
