bc44c0ba7a83860fc0f55beac790311a1f8ece5d
taha-tech-2003 <majorcop17@gmail.com>
2025-08-06 21:14:37 +0500

    con

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
