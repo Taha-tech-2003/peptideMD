8f5bf4e103aacfa09af3802bfec41289ae55b301
taha-tech-2003 <majorcop17@gmail.com>
2025-08-06 21:31:44 +0500

    conome

--- next.config.ts
+++ next.config.ts
@@ -12,20 +12,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,  // ✅ Disable Image Optimization for static export
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true, // ✅ Disable ESLint from failing the build
-//   },
-//   experimental: {
-//     scrollRestoration: true,
-//   },
-// };
-
-// export default nextConfig;
